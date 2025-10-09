7e858aea2c2780bd5604be317972e522e2b58415
Marsel <miorauz@gmail.com>
2025-10-09 11:42:25 +0200

    Initial Commit

---
+++ oneLoop/oneLoopApp.swift
@@ -0,0 +1,17 @@
+//
+//  oneLoopApp.swift
+//  oneLoop
+//
+//  Created by Marsel Khasanov on 09/10/25.
+//
+
+import SwiftUI
+
+@main
+struct oneLoopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
