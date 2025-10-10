1f8506bac59438bb7e59296082eb3c986bf68974
Chemi <josemiguel.guerrerojim@gmail.com>
2025-10-10 10:21:55 +0200

    Views and Components Folders

---
+++ oneLoop/Components/ChallengeCard.swift
@@ -0,0 +1,18 @@
+//
+//  ChallengeCard.swift
+//  oneLoop
+//
+//  Created by José Miguel Guerrero Jiménez on 10/10/25.
+//
+
+import SwiftUI
+
+struct ChallengeCard: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ChallengeCard()
+}
---
+++ oneLoop/Views/HomeView.swift
@@ -0,0 +1,18 @@
+//
+//  HomeView.swift
+//  oneLoop
+//
+//  Created by José Miguel Guerrero Jiménez on 10/10/25.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HomeView()
+}
